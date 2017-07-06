8dcc11bc7a399ad06ea76205a66092e24da44e92
Cameron Thompson <camo.thommo@gmail.com>
2017-07-06 22:30:59 +1000

    Added Social Media
    Added discord account to the social media

--- CameronThompsonWebsite/javaScript.js
+++ CameronThompsonWebsite/javaScript.js
@@ -14,38 +14,44 @@ function ChangeImageSize(bigger){
 	}
 }
 
-function FacebookImageHighlight(TF){
-	facebookPicture = document.getElementById("FacebookImage");
-	facebookName = document.getElementById("FacebookName");
+function SocialMediaHighlight(TF, ImageID, NameID, PicPath, HoverPicPath){
+	socialMediaPicture = document.getElementById(ImageID);
+	socialMediaName = document.getElementById(NameID);
 	
 	if (TF){
-		facebookPicture.src = "Images/FacebookLogoHover.png";
-		facebookName.style.visibility = "visible";
+		socialMediaPicture.src = HoverPicPath;
+		socialMediaName.style.visibility = "visible";
 	} else {
-		facebookPicture.src = "Images/FacebookLogo.png";
-		facebookName.style.visibility = "hidden";
+		socialMediaPicture.src = PicPath;
+		socialMediaName.style.visibility = "hidden";
 	}
+}
+
+function FacebookImageHighlight(TF){
+	SocialMediaHighlight(TF, "FacebookImage", "FacebookName", 
+			"Images/FacebookLogo.png", "Images/FacebookLogoHover.png");
 } 
+
+function GitHubImageHighlight(TF){
+	SocialMediaHighlight(TF, "GitHubImage", "GitHubName", 
+			"Images/GitHubLogo.png", "Images/GitHubLogoHover.png");
+}
+
+function DiscordImageHighlight(TF){
+	SocialMediaHighlight(TF, "DiscordImage", "DiscordName",
+			"Images/Discord.png", "Images/DiscordHover.png");
+}
 
 function OpenFacebook(){
 	window.open("https://www.facebook.com/cameron.thompson.169");
 }
 
-function GitHubImageHighlight(TF){
-	GitHubPicture = document.getElementById("GitHubImage");
-	GitHubName = document.getElementById("GitHubName");
-	
-	if (TF){
-		GitHubPicture.src = "Images/GitHubLogoHover.png";
-		GitHubName.style.visibility = "visible";
-	} else {
-		GitHubPicture.src = "Images/GitHubLogo.png";
-		GitHubName.style.visibility = "hidden";
-	}
-}
-
 function OpenGitHub(){
 	window.open("https://github.com/chaosdrake");
+}
+
+function OpenDiscord(){
+	window.open("https://discordapp.com");
 }
 
 function DisplayInfo(TF){
